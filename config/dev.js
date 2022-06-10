8fa20c3adaf431de87e1530662a1cf9a410e6e22
Noa <noashohat1996@gmail.com>
2022-06-10 12:43:23 +0300

    mongodb config

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,13 @@
 module.exports = {
+  //mySql
   DB_HOST: "localhost",
   DB_USER: "root",
   DB_PASSWORD: "",
   DB_NAME: "royal_crm",
+
+  //mongoDB
+  MONGO_DB: "royalCrm",
+  MONGO_HOST: "mongodb://localhost:27017",
+  //auth
   JWT_SECRET: "mysecretkey",
 };
