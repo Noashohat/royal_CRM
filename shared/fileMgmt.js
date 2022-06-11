29780268ec57dcf9cca8cebddacf8f8836847483
Noa <noashohat1996@gmail.com>
2022-06-11 21:37:53 +0300

    file magmt

--- shared/fileMgmt.js
+++ shared/fileMgmt.js
@@ -1,14 +1,16 @@
 const path = require("path");
 const fs = require("fs");
-const mongo = require("../controllers/database");
+const database = require("../controllers/database");
 
 module.exports = {
   getHtmlFilePath: function (htmlFileName) {
     return path.join(__dirname, "../client", htmlFileName);
   },
 
-  exportToFile: async function (res, collectionName) {
+  exportToFile: async function (res, sql, filePrefix) {
     try {
+      const result = await database.query(sql);
+
       const now = new Date().getTime(); // moment.js
       const filePath = path.join(
         __dirname,
@@ -16,25 +18,15 @@ module.exports = {
         `${filePrefix}-${now}.txt`
       );
       const stream = fs.createWriteStream(filePath);
-      const database = await mongo.getDb();
-      const collection = database.collection(collectionName);
-      const cursor = collection.find({});
-      await cursor.forEach((doc) => {
-        stream.write(JSON.stringify(doc));
-      });
-      stream.end();
-      stream.on("close", function () {
-        res.send(`success. file at: ${filepath}`);
+
+      stream.on("open", function () {
+        stream.write(JSON.stringify(result[0]));
+        stream.end();
       });
 
-      // stream.on("open", function () {
-      //   stream.write(JSON.stringify(result[0]));
-      //   stream.end();
-      // });
-
-      // stream.on("finish", function () {
-      //   res.send(`Success. File at: ${filePath}`);
-      // });
+      stream.on("finish", function () {
+        res.send(`Success. File at: ${filePath}`);
+      });
     } catch (err) {
       throw err;
     }
